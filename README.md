61dc4949044977eaf56801a00a85c1cfe9068cb9
Fyodor Yakimchouk <fyodor.yakimchouk@clearpoint.co.nz>
2016-10-02 19:22:11 +1300

    Updating deployment configuration instead of individual pods.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Kubeimage
 
-A small cli utility to get/update images in Kubernetes.
+A small cli utility to get/update deployment images in Kubernetes.
 
 ## Installation
 
@@ -11,9 +11,10 @@ npm i kubeimage -g
 ## Usage:
 
 ```
-kubeimage <podname>=[<new-build-number>]... [--namespace=<kube-namespace>] [--kubeconfig=<kubeconfig>]
+kubeimage <podname>=[<new-build-number>]... [--timeout=600] [--namespace=<kube-namespace>] [--kubeconfig=<kubeconfig>]
 ```
 
-When no build number is provided the tool will output current build number.
+Timeout (in seconds, default 600) is amount of time that tool waits for pods to come up before it fails with error.
+When no build number is provided the tool will output current build number for deployment and pods.
 Note: images have to follow `build-<number>` naming convention.
 
--- kubeimage.js
+++ kubeimage.js
@@ -14,6 +14,20 @@ function timeLog (line) {
     console.log(`[${chalk.grey(timestamp('HH:mm:ss'))}] ${line}`);
 }
 
+function parseDeployments (stdout) {
+    let m;
+    const re = /^([^\s]+)[\s]+([\d]+)/gm;
+    const deployMents = [];
+    while (m = re.exec(stdout)) {
+        deployMents.push({
+            name: m[1],
+            desiredCount: m[2]
+        });
+    }
+    return deployMents;
+
+}
+
 function parsePods (stdout) {
     let m;
     const re = /([^\s]+)[\s]+([\d]*)\/([\d]*)[\s]+([^\s]+)[\s]+([\d]*)/gm;
@@ -42,8 +56,18 @@ function getPods (callback) {
         }
     });
 }
+function getDeployments (callback) {
+    exec(`kubectl get deployments${ kubeConfig ? ' --kubeconfig=' + kubeConfig : ''}${ namespace ? ' --namespace=' + namespace : ''}`, function (error, stdout, stderr) {
+        if (error || stderr) {
+            timeLog(chalk.red(`Network error when getting deployments`));
+            callback(true);
+        } else {
+            callback(false, parseDeployments(stdout));
+        }
+    });
+}
 
-function getNiceState(pod) {
+function getNiceState (pod) {
     let r = pod.ready ? chalk.green('Ready') : chalk.red('Not ready');
     r += ', ';
     switch (pod.state) {
@@ -64,7 +88,7 @@ function getNiceState(pod) {
     return r;
 }
 
-function getExtraParams() {
+function getExtraParams () {
     return `${ kubeConfig ? ' --kubeconfig=' + kubeConfig : ''}${ namespace ? ' --namespace=' + namespace : ''}`;
 }
 
@@ -85,40 +109,117 @@ function getPodBuildNumber (podId, callback) {
     });
 }
 
-function updatePodBuildNumber (pod, buildNumber, originalBuild) {
-    exec(`kubectl get pod ${pod.id} -o yaml${getExtraParams()} | sed 's/\\(image: .*\\):.*$/\\1:build-${buildNumber}/' | kubectl${getExtraParams()} replace -f -`,  function (error, stdout, stderr) {
+function getDeploymentBuildNumber (serviceName, callback) {
+    exec(`kubectl get deployment ${serviceName} -o yaml${getExtraParams()}`, function (error, stdout, stderr) {
+        if (error || stderr) {
+            timeLog(`Network error when getting pod number`);
+            console.error(error, stderr);
+            callback(true);
+        } else {
+            const match = /image:.*-([\d]*)/.exec(stdout);
+            if (match) {
+                callback(false, match[1]);
+            } else {
+                callback(true);
+            }
+        }
+    });
+}
+
+let pollingStarted = false;
+let pollingTargets = [];
+let pollingListeners = [];
+let pollingHasErrors = false;
+let pollInterval = 2000;
+let pollTimeout = 600;
+let pollStarted;
+
+function pollPods() {
+
+    if (!pollStarted) {
+        pollStarted = Math.floor(new Date() / 1000);
+    }
+
+    if (Math.floor(new Date() / 1000) - pollStarted > pollTimeout) {
+        timeLog(chalk.red(`Timeout reached while waiting for pods to restart for deployments: ${pollingTargets.join(', ')}`));
+        process.exit(1);
+    }
+
+    getPods((error, pods) => {
+        if (!error) {
+            pollingListeners.forEach(listener => {
+                listener(pods);
+            });
+        }
+        if (pollingTargets.length == 0) {
+            finishUpdate();
+        } else {
+            setTimeout(pollPods, pollInterval);
+        }
+    });
+}
+
+function finishUpdate() {
+    if (pollingHasErrors) {
+        process.exit(1);
+    }
+    timeLog(`Completed updating deployments${!pollingHasErrors ? ' (no errors)' : ''}`);
+    process.exit();
+}
+
+function updateDeploymentBuildNumber (deployment, buildNumber, originalBuild, oldPods) {
+    const serviceName = deployment.name;
+    exec(`kubectl get deployment ${serviceName} -o yaml${getExtraParams()} | sed 's/\\(image: .*\\):.*$/\\1:build-${buildNumber}/' | kubectl${getExtraParams()} replace -f -`, function (error, stdout, stderr) {
         if (error || stderr) {
             timeLog(`Error while updating ${pod.id}`);
+            _.pull(pollingTargets, serviceName);
+            pollingHasErrors = true;
             console.error(error, stderr);
+            if (!pollingTargets.length) {
+                finishUpdate();
+            }
         } else {
-            timeLog(`Pod ${chalk.cyan(pod.id)} has been updated from build ${chalk.magenta(originalBuild)} to build ${chalk.magenta(buildNumber)}, waiting for it to restart...`);
+            timeLog(`Deployment ${chalk.cyan(serviceName)} has been updated from build ${chalk.magenta(originalBuild)} to build ${chalk.magenta(buildNumber)}, waiting for pods to restart...`);
 
-            const checkIfPodIsUp = () => {
-                getPods((error, pods) => {
-                   if (!error) {
-                       const updatedPod = _.find(pods, { id: pod.id });
-                       if (!updatedPod) {
-                           timeLog(`Pod ${chalk.cyan(pod.id)} was eliminated!`);
-                       } else {
-                           if (updatedPod.restarts > pod.restarts || pod.state != updatedPod.state || pod.ready != updatedPod.ready) {
-                               if (updatedPod.state == 'Error' || updatedPod.state == 'CrashLoopBackOff') {
-                                   timeLog(`Pod ${chalk.cyan(pod.id)} went into ${chalk.red(updatedPod.state)} state!`);
-                               } else {
-                                   if (updatedPod.state = 'Running' && updatedPod.ready) {
-                                       timeLog(`Pod ${chalk.cyan(pod.id)} has been restarted and is now running build ${chalk.magenta(buildNumber)}`);
-                                   } else {
-                                       setTimeout(checkIfPodIsUp, 2000);
-                                   }
-                               }
-                           } else {
-                               setTimeout(checkIfPodIsUp, 2000);
-                           }
-                       }
-                   }
+            const listener = (pods) => {
+                const currentPods = _.filter(pods, (pod) => {
+                    return pod.id.toLowerCase().startsWith(serviceName);
                 });
+
+                _.pullAllBy(currentPods, oldPods, 'id');
+
+                let totalPodsRunning = 0;
+                let totalErrors = 0;
+
+                if (currentPods.length) {
+                    currentPods.forEach(pod => {
+                        if (pod.state !== 'ContainerCreating' && pod.state !== 'Running' && pod.state !== 'Pending') {
+                            timeLog(`Pod ${chalk.cyan(pod.id)} went into ${chalk.red(pod.state)} state!`);
+                            totalErrors++;
+                        } else if (pod.state == 'Running' && pod.ready) {
+                            totalPodsRunning++;
+                        }
+                    })
+                }
+
+                if (totalPodsRunning + totalErrors == deployment.desiredCount) {
+                    if (totalErrors > 0) {
+                        pollingHasErrors = true;
+                        timeLog(`${totalErrors} of ${deployment.desiredCount} pod${totalErrors > 0 ? 's' : ''} failed to restart for ${chalk.cyan(serviceName)}`);
+                    } else {
+                        timeLog(`All pods for ${chalk.cyan(serviceName)} have been restarted`);
+                    }
+                    _.pull(pollingTargets, serviceName);
+                    _.pull(pollingListeners, listener);
+                }
             };
 
-            setTimeout(checkIfPodIsUp, 2000);
+            pollingListeners.push(listener);
+
+            if (!pollingStarted) {
+                pollingStarted = true;
+                pollPods();
+            }
         }
     });
 
@@ -126,19 +227,21 @@ function updatePodBuildNumber (pod, buildNumber, originalBuild) {
 
 try {
     const args = process.argv.slice(2);
-    let podsToUpdate = [];
+    let deploymentsToUpdate = [];
 
     args.forEach(arg => {
         if (arg.match(/--kubeconfig=[^\s]*?/)) {
             kubeConfig = arg.split('=')[1];
         } else if (arg.match(/--namespace=[a-zA-z]*?/)) {
             namespace = arg.split('=')[1];
+        } else if (arg.match(/--timeout=[a-zA-z]*?/)) {
+            pollTimeout = arg.split('=')[1];
         } else {
-            podsToUpdate.push(arg.split('='));
+            deploymentsToUpdate.push(arg.split('='));
         }
     });
 
-    if (podsToUpdate.length == 0) {
+    if (deploymentsToUpdate.length == 0) {
         throw 'No pod names provided';
     }
 
@@ -150,41 +253,47 @@ try {
         timeLog(`Using namespace ${namespace}`);
     }
 
-    getPods((error, pods) => {
+    getDeployments((error, deployments) => {
         if (error) {
-            process.exit();
+            process.exit(1);
         } else {
-            podsToUpdate.forEach(podInfo => {
-                const podIds = _.filter(pods, (pod) => {
-                    return pod.id.toLowerCase().startsWith(podInfo[0]);
-                });
-                if (podIds.length) {
+            deploymentsToUpdate.forEach(deployment => {
+                const foundDeployment = _.find(deployments, { name: deployment[0] } );
+                if (foundDeployment) {
 
-                    const uniqueNames = _.uniqBy(podIds, 'name');
+                    const serviceName = deployment[0];
 
-                    if (uniqueNames.length > 1) {
-                        timeLog(chalk.red(`More than one pod matches '${podInfo[0]}': ${(_.flatMap(uniqueNames, 'name')).join(', ')}`));
-                    } else {
-                        podIds.forEach(podId => {
-                            getPodBuildNumber(podId.id, (error, buildNumber)=> {
-                                if (error) {
-                                    timeLog(chalk.red(`Cannot get build number for pod ${podId.id}`));
+                    getPods((error, pods) => {
+                        if (error) {
+                            process.exit(1);
+                        }
+                        pods = _.filter(pods, { name: deployment[0]});
+                        if (deployment.length > 1) {
+                            getDeploymentBuildNumber(serviceName, (error, buildNumber)=> {
+                                if (deployment[1] == buildNumber) {
+                                    timeLog(`Deployment ${chalk.cyan(serviceName)} is already configured to build ${chalk.magenta(buildNumber)}`);
                                 } else {
-                                    if (podInfo.length > 1) {
-                                        if (podInfo[1] == buildNumber) {
-                                            timeLog(`Pod ${chalk.cyan(podId.id)} is already on build ${chalk.magenta(buildNumber)}`);
-                                        } else {
-                                            updatePodBuildNumber(podId, podInfo[1], buildNumber);
-                                        }
-                                    }  else {
-                                        timeLog(`Pod ${chalk.cyan(podId.id)} (${getNiceState(podId)}) is on build ${chalk.magenta(buildNumber)}`);
-                                    }
+                                    pollingTargets.push(deployment[0]);
+                                    updateDeploymentBuildNumber(foundDeployment, deployment[1], buildNumber, pods);
                                 }
                             });
-                        })
-                    }
+                        } else {
+                            getDeploymentBuildNumber(serviceName, (error, buildNumber)=> {
+                                timeLog(`Deployment ${chalk.cyan(serviceName)} is configured to build ${chalk.magenta(buildNumber)}`);
+                                pods.forEach(pod => {
+                                    getPodBuildNumber(pod.id, (error, buildNumber)=> {
+                                        if (error) {
+                                            timeLog(chalk.red(`Cannot get build number for pod ${pod.id}`));
+                                        } else {
+                                            timeLog(`Pod ${chalk.cyan(pod.id)} (${getNiceState(pod)}) is running build ${chalk.magenta(buildNumber)}`);
+                                        }
+                                    });
+                                })
+                            });
+                        }
+                    });
                 } else {
-                    timeLog(chalk.red(`Could not find any pod that start with ${podInfo}`));
+                    timeLog(chalk.red(`Could not find '${deployment}' deployment`));
                 }
             });
         }
